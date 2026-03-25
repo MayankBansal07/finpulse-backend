1290b09d938026a811d02f4f319ee9d8873ed72c
MayankBansal07 <maybansalexams@gmail.com>
2026-03-25 10:36:12 +0530

    fix smtp ipv6 issue

--- server.js
+++ server.js
@@ -18,11 +18,16 @@ app.get("/", (req, res) => {
 // Configure Nodemailer Transporter
 // Use environment variables for security in production
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 app.post('/contact', async (req, res) => {
